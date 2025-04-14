2fa52fdf1be5897f690ca9a4194f98d95fe94b46
M Hassan BIKIENGA <m9bikienga@gmail.com>
2025-04-14 10:30:12 +0000

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+            <h1 className=""> Burger House</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
